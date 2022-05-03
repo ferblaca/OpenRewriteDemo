61bf56e4750d71992e0e3d1e0622292290a99470
ferblaca <fernandobc@ext.inditex.com>
2022-05-03 10:30:57 +0200

    initial commit

---
+++ src/test/java/com/example/openrewrite/test/demoOpenrewrite/DemoOpenrewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openrewrite.test.demoOpenrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenrewriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
